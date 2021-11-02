45a1fb81f526ac78e3be39915aec7b732b4fe950
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-02 00:28:14 -0400

    commit tree 619bcf673a09e6afc7019e2fa04d23cb580fc99c

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(travel){
+  if (travel <= 400){
+    return 'This one is on me!'
+  } else if (travel >= 2000 && travel < 2500) {
+      return `I will gladly take your thirty bucks.`
+  } else 
+      return 'No can do.'
+  } 
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+} 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){  
+  switch (tip){
+    case 'generous': return 'Thank you so much.';
+    case 'not as generous': return 'Thank you.';
+    case 'thanks for everything': return 'Bye.';
+  }
+};
